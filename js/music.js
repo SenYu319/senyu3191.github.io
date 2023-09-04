daff4ede0207ebae11a684d83c1ae2da5111bd22
SenYu319 <106578131+SenYu319@users.noreply.github.com>
2023-09-04 18:11:44 +0800

    Update music.js

--- js/music.js
+++ js/music.js
@@ -10,8 +10,8 @@ GitHub：https://github.com/imsyy/home
 版权所有，请勿删除
 */
 var server = "netease"; //netease: 网易云音乐; tencent: QQ音乐; kugou: 酷狗音乐; xiami: 虾米; kuwo: 酷我
-var type = "playlist"; //song: 单曲; playlist: 歌单; album: 唱片
-var id = "7452421335"; //封面 ID / 单曲 ID / 歌单 ID
+var type = "song"; //song: 单曲; playlist: 歌单; album: 唱片
+var id = "1496132290"; //封面 ID / 单曲 ID / 歌单 ID
 
 $.ajax({
     url: "https://api.wuenci.com/meting/api/?server=" + server + "&type=" + type + "&id=" + id, //json文件位置，文件名
